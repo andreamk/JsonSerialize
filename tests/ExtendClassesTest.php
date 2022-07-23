9875a7ef108a72fe2fc2f39ba67375640a3134de
Andrea Leoni <a.leoni@amkdeveloper.com>
2022-07-23 13:26:25 +0200

    unit test fixed

--- tests/ExtendClassesTest.php
+++ tests/ExtendClassesTest.php
@@ -101,9 +101,7 @@ final class ExtendClassesTest extends TestCase
         $serializedValue = json_encode($list, JSON_PRETTY_PRINT);
         $this->assertTrue(is_string($serializedValue), 'Value is string');
         $unserializedValue = JsonSerialize::unserialize($serializedValue);
-        $this->assertTrue(is_array($serializedValue), 'Value is array');
-
-        $this->assertEquals(is_array($unserializedValue), true, "check if resource after unserilized object is array");
+        $this->assertTrue(is_array($unserializedValue), "check if resource after unserilized object is array");
         $this->assertEquals($numElements, count($unserializedValue), "check if resource after unserilized object is initialized");
 
         for ($i = 0; $i < $numElements; $i++) {
